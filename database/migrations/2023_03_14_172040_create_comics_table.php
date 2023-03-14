14b77d6556c5cd5404ce119e2e6d4d69da0b4170
Salvatore Capano <capano_salvatore@libero.it>
2023-03-14 18:38:10 +0100

    Milestone 1 Completata

---
+++ database/migrations/2023_03_14_172040_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 64);
+            $table->string('thumb', 124);
+            $table->decimal('price', $precision = 6, $scale = 2);
+            $table->string('series', 64);
+            $table->date('sale_date');
+            $table->enum('type', ['comic book', 'graphic novel']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
